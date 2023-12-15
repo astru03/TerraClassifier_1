7ec24ba810db7b1e5c2d9369f971b1a11fd3184c
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2023-12-15 16:56:22 +0100

    Code comments added

--- TerraClassifier_backend/server_backend.js
+++ TerraClassifier_backend/server_backend.js
@@ -7,18 +7,17 @@ const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// Middleware für CORS aktivieren
+// Enable middleware for CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Hier können die erlaubten Origin-Domains spezifiziert werden
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // The permitted origin domains can be specified here
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
 
 app.post('/satellite', (req, res) => {
-  //check if Date and Coordinates not null
+  //check if Date, Coordinates and Cloud-cover is not null
   if(req.body.Date == '' || req.body.NEC == '' || req.body.SWC == '' || req.body.CCI == ''){
-    //res.sendFile(reqpath + "/public/error_empty_input.html")
     console.log('Fehler Felder nicht gefüllt')
   return;
   }
@@ -26,60 +25,55 @@ app.post('/satellite', (req, res) => {
   let receivedNEC = req.body.NEC;
   let receivedSWC = req.body.SWC;
   let receivedCCI = req.body.CCI;
-  // Beispiel: Wenn die Koordinaten im Terminal ausgegeben werden sollen
-  console.log(receivedDate);
-  console.log(receivedNEC);
-  console.log(receivedSWC);
-  console.log(receivedCCI);
 
-//Aus den NEC und SWC muss ein polygonCoordinates gemacht werden. Das muss noch dynamisch funktionieren
-  let SplitReceivedNEC = receivedNEC.split(",") //Aufspalten am Komma um auch die Koordinaten für NWC und SEC zu erhalten
-  let SplitReceivedSWC = receivedSWC.split(",") //Die sind nötig um weiter unten das Rechteckt aufzubauen
+// A polygonCoordinates must be made from the northeast coordinates and the southwest coordinates.
+  let SplitReceivedNEC = receivedNEC.split(",") // Split at the comma to also get the coordinates for NWC and SEC
+  let SplitReceivedSWC = receivedSWC.split(",") // These are necessary to build the rectangle polygon
   let stringNEC = [SplitReceivedNEC[0], SplitReceivedNEC[1].trim()] 
   let stringNWC = [SplitReceivedSWC[0], SplitReceivedNEC[1].trim()];
   let stringSWC = [SplitReceivedSWC[0], SplitReceivedSWC[1].trim()]
   let stringSEC = [SplitReceivedNEC[0], SplitReceivedSWC[1].trim()];
-  let NEC = stringNEC.map(parseFloat);  //um aus Array von Strings ein Array aus Gleitkommazahlen zu machen
+  // to turn array of strings into array of floating point numbers
+  let NEC = stringNEC.map(parseFloat);
   let NWC = stringNWC.map(parseFloat);
   let SWC = stringSWC.map(parseFloat);
   let SEC = stringSEC.map(parseFloat);
 
-  // Koordinaten von AOI und AOT in liste pushen.
+  // Koordinaten von AOI und AOT in liste pushen. ?????????????
   
-//Das Datum muss an den searchbody übergeben werden. Das muss noch dynamisch funktionieren
+  // STAC API from AWS S3
   const api_url = 'https://earth-search.aws.element84.com/v1';
   const collection = 'sentinel-2-l2a'; // Sentinel-2, Level 2A, Cloud Optimized GeoTiffs (COGs)
  
   let polygonCoordinates = [
-    NEC, //Nordosten
-    SEC, //Südosten
-    SWC, //Südwesten
-    NWC, //Nordwesten
-    NEC, //Nordosten
+    NEC, // Northeast
+    SEC, // southeast
+    SWC, // southwest
+    NWC, // Northwest
+    NEC, // Northeast
   ];
-  console.log(polygonCoordinates);
+  // create a polgongeojson
   let polygonGeoJSON = {
     "type": "Polygon",
     "coordinates": [polygonCoordinates],
   };
 
-  //Datum formatieren
-  let dateParts = receivedDate.split('.') //Aufspalten des Datums
-  let newDate = new Date(dateParts[2],dateParts[1] - 1, dateParts[0]); //Vorsicht Monate starten bei 0. Also Janua = 0 deswegen -1 bei Monat
+  // Format date
+  let dateParts = receivedDate.split('.') // Splitting the old date format
+  let newDate = new Date(dateParts[2],dateParts[1] - 1, dateParts[0]); // Be careful months start at 0. So Janua = 0 therefore -1 for month
   let year = newDate.getFullYear();
-  let month = String(newDate.getMonth() + 1).padStart(2, '0'); // Führende Nullen für Monat hinzufügen
-  let day = String(newDate.getDate()).padStart(2, '0'); // Führende Nullen für Tag hinzufügen
+  let month = String(newDate.getMonth() + 1).padStart(2, '0'); // Add leading zeros for month
+  let day = String(newDate.getDate()).padStart(2, '0'); // Add leading zeros for tag
   let NewStartDate = `${year}-${month}-${day}`;
 
-
-  let startDate = new Date(NewStartDate); //Hier kommt ein komisches format raus z.b. 2023-12-03T00:00:00.000Z
-  startDate.setDate(startDate.getDate() + 14); // zu dem format wird 14 Tage zum Startdatum hinzufügen
-  let endDate = startDate.toISOString().split('T')[0]; // Formatieren damit nur noch das Format YYYY-MM-DD vorliegt
+  let startDate = new Date(NewStartDate); // The format “2023-12-03T00:00:00.000Z” comes out here
+  startDate.setDate(startDate.getDate() + 14); // to the selected date will add 14 days to the start date
+  let endDate = startDate.toISOString().split('T')[0]; // Format so that only the format YYYY-MM-DD is available
   let startTime = 'T00:00:00Z';
   let endTime = 'T23:59:59Z';
   let date = NewStartDate + startTime + '/' + endDate + endTime;
-  //console.log(date);
 
+  // searchbody for obtaining the geotiffs
   let searchBody = { 
     collections: [collection],
     intersects: polygonGeoJSON,
@@ -90,9 +84,7 @@ app.post('/satellite', (req, res) => {
     }}
   };
 
-  console.log(searchBody);
-
-  //Fetch der API um die Bilder zu holen
+  // Fetch the STAC API to get the geotiff of the satellite images
   fetch(`${api_url}/search`, {
     method: 'POST',
     headers: {
@@ -105,12 +97,10 @@ app.post('/satellite', (req, res) => {
     .then((data) => {
       console.log(data.context);
       const items = data.features;
-      console.log(items.length); //Wieviele wurden gefunden nach den kriterien
+      console.log(items.length); // How many satellite images were found according to the specified search criteria
       
-      let objSatellitenImages = {}; //in diesem Objekt werden die id, die url und die imageBounds der resulate gespeichert
+      let objSatellitenImages = {}; // The id, the url and the imageBounds of the results are stored in this object
 
-      
-      
       for (var index = 0; index < items.length; index ++) {
         //let itemID = items[index].id  //So kommt man an die items.id
         //console.log(itemID);
@@ -125,26 +115,23 @@ app.post('/satellite', (req, res) => {
         
         objSatellitenImages['item_' + index] = {
           id: items[index].id, 
-          //url: items[index].assets.thumbnail.href,
-          url: items[index].assets.visual.href,
+          //url: items[index].assets.thumbnail.href, // To get the URL for the thumbnails
+          url: items[index].assets.visual.href, //To get the URL for the geotiffs
           imageBounds: items[index].geometry.coordinates}
       }
       
-      console.log(objSatellitenImages);
-      
-      //Objekt wird zurückgegeben an das Frontend
+      // Object "objSatellitenImages" is returned to the frontend
       if (objSatellitenImages != null ) {
         res.json(objSatellitenImages)
       } else {
         res.status(400).json({ error: 'Ungültige Anfrage' });
       }
-
     })
     .catch((error) => console.error('Error:', error));
 });
 
 
-//Listener
+// Listener
 app.listen(port, () => {
     console.log(`Backend Service listening at http://localhost:${port}`)
   });
