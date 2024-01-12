20afcc18c5c29f37dc25292f4a366f9c68d72326
starke01 <starke331@icloud.com>
2024-01-12 16:58:56 +0100

    finish

--- terraclassifier_app/server_app.js
+++ terraclassifier_app/server_app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000
-//const fetch = require('node-fetch');
-import fetch from 'node-fetch'
+const fetch = require('node-fetch');
 
 const fs = require('fs');
 const multer = require('multer')
